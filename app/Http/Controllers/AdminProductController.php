585d8a65d595fb0ce134b394f023cf916568bead
Khangug <giapvankhang6789@gmail.com>
2024-06-19 03:19:37 +0700

    tags

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -4,19 +4,28 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Components\Recusive;
 use App\Models\Product;
+use App\Models\ProductImage;
+use App\Models\ProductTag;
 use App\Traits\StorageImageTrait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
+use App\Models\Tag;
 
 class AdminProductController extends Controller
 {      
     use StorageImageTrait;
     private $category;
     private $product;
-    public function __construct(Category $category, Product $product){
+    private $productImage;
+    private $tag;
+    private $productTag;
+    public function __construct(Category $category, Product $product, ProductImage $productImage, Tag $tag, ProductTag $productTag){
         $this->category = $category;
         $this->product = $product;
+        $this->productImage = $productImage;
+        $this->tag= $tag;
+        $this->productTag = $productTag;
     }
     public function index(){
         return view('admin.product.index');
@@ -53,6 +62,32 @@ class AdminProductController extends Controller
     }
 
     $product = $this->product->create($dataProductCreate);
+
+    //insert vao product image
+
+    if($request->hasFile('image_path')){
+        foreach($request->image_path as $fileItem){
+            $dataProductImageDetail = $this->storageTraitUploadMutiple($fileItem,'products');
+            $product->images()->create([
+                'product_id' =>$product->id,
+                'image_path' => $dataProductImageDetail['file_path'],
+                'image_name' => $dataProductImageDetail['file_name'],
+
+            ]);
+            
+        }
+    }
+
+    //insert tag for product
+
+    foreach($request->tags as $tagItem){
+        //Insert ddeesn bang tag
+        $tagInstance = $this->tag->firstOrCreate(['name' => $tagItem]);
+        $tagIds[] = $tagInstance->id;
+    }
+    $product->tags()->attach($tagIds);
+
+
     
 }
 
--- app/Traits/StorageImageTrait.php
+++ app/Traits/StorageImageTrait.php
@@ -22,4 +22,20 @@ trait StorageImageTrait {
         
         return null;
     }
+
+    
+    public function storageTraitUploadMutiple($file, $folderName) {
+        
+            $fileNameOrigin = $file->getClientOriginalName();
+            $fileNameHash = Str::random(20) . '.' . $file->getClientOriginalExtension(); // Sử dụng Str::random()
+            $filePath = $file->storeAs('public/' . $folderName . '/' . auth()->id(), $fileNameHash);
+    
+            $dataUploadTrait = [
+                'file_name' => $fileNameOrigin,
+                'file_path' => Storage::url($filePath), // Sử dụng dấu nháy kép cho biến $filePath
+            ];
+            return $dataUploadTrait;
+        
+        
+    }
 }
